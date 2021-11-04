70d6035d203dd4289f094d47e85a33d27733eed4
CelesteSMThomas <celestesmthomas@gmail.com>
2021-11-04 14:13:57 -0700

    created create and get_current_user methods/routes

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,26 @@
+class Api::V1::SessionsController < ApplicationController
+    def create
+       @user = User.find_by(username: params[:session][:username])
+
+       if @user && @user.authenticate(params[:session][:password])
+        render json: @user
+        session[:user_id] = @user.id
+       else
+        render json: {
+            error: "Invalid Credentials"
+        }
+       end
+    end
+
+def get_current_user
+    if logged_in?
+        render json: current_user
+    else
+        render json:
+        error: "No one is logged in"
+    end
+
+end
+
+
+end
